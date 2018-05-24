75ad90e69268496c998ce46af68fe81f8883e052
jcase <1974720936@qq.com>
2018-05-24 11:37:24 +0800

    更新

--- firstweb/src/main/java/com/example/firstweb/FirstwebApplication.java
+++ firstweb/src/main/java/com/example/firstweb/FirstwebApplication.java
@@ -1,7 +1,9 @@
 package com.example.firstweb;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -10,28 +12,23 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class FirstwebApplication {
-	@Bean ApplicationRunner runner() {
-		return new ApplicationRunner() {
-			
-			@Override
-			public void run(ApplicationArguments args) throws Exception {
-				// TODO Auto-generated method stub
-				System.out.println("Hello , spring bean"); 
-			}
+	@Autowired
+	ApplicationContext context;
+	
+	@Bean
+	public CommandLineRunner runner() {
+		return (args) -> {
+			int count = context.getBeanDefinitionCount();
+	        System.out.println("Beans的个数：" + count);
+	        String[] names = context.getBeanDefinitionNames();
+	        for (String name : names) {
+	        		System.out.printf("%s -> %s\n", name, context.getBean(name));
+	        }
 		};
 	}
 	public static void main(String[] args) {
-		//SpringApplication.run(FirstwebApplication.class, args);
-		ApplicationContext context = SpringApplication.run(FirstwebApplication.class, args);
-        int count = context.getBeanDefinitionCount();
-        System.out.println("Beans的个数：" + count);
-        String[] names = context.getBeanDefinitionNames();
-        for (String name : names) {
-               System.out.print(name);
-               System.out.print("->");
-               Object bean = context.getBean(name);
-               System.out.println(bean.getClass().getName());
-        }
+		SpringApplication.run(FirstwebApplication.class, args);
+		
 	}
 	
 }
